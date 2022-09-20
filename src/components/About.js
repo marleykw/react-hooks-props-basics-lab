85bd5938af39d3b34ff5ba7ec1768834b32927ea
Marley Watson <marleykw17@gmail.com>
2022-09-20 15:43:08 -0400

    commit tree ae717892a6085fccaa3a6a8d486a83b550623ee4

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,33 @@
 import React from "react";
+import Links from "./Links";
+//console.log(user.links.github)
 
-function About() {
+
+function About(user) {
+ const bio = user.bio
+ //const bioExist= bio.length
+ //console.log(bio)
+ //if (bio === undefined || bio === null || bio == ' ') {
+  if (typeof bio != 'undefined' && bio) {
+   console.log(`${bio} is not undefined or blank`)
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{user.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
+  } else {
+    console.log(`${bio} is undefined or blank`)
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github={user.links.github} linkedin={user.links.linkedin}/>
+      </div>
+    );
+  }
 }
 
 export default About;
